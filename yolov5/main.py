28c6d8181913bb3c59fa5a41042376ea9ecc67f7
jin-you <58108092+jin-you@users.noreply.github.com>
2021-10-01 00:20:48 +0900

    Update main.py
    flask

--- yolov5/main.py
+++ yolov5/main.py
@@ -1,75 +1,72 @@
-import json
-import cv2
-import requests
+from flask import Flask, request
+from PIL import Image
+import io
 import torch
+import requests
 from bs4 import BeautifulSoup as bs
-
 import search_calorie as sc
+import json
 
 
-def detect_food():
-    # img
-    img_PATH = 'IMAGE_PATH'
+app=Flask(__name__)
 
-    # Model Load('Yolov5 경로', 'custom = 커스텀 모델임', path='모델 가중치파일 경로', source='local' 로컬에 저장된 것임)
-    model = torch.hub.load('./models/yolo.py', 'custom', path='WEIGHT_PATH', source='local')  # local repo
+def prediction(img_file) :
 
-    # Images load
-    img = cv2.imread(img_PATH)[:, :, ::-1]  # OpenCV image (BGR to RGB)
-    imgs = [img]  # batch of images
+    img_bytes=img_file.read()
+    img = Image.open(io.BytesIO(img_bytes))
 
-    # Inference
-    results = model(imgs, size=640)  # includes NMS
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Users/user/PycharmProjects/food_detect_server/best_model.pt')
 
-    # Results
-    # results.print()
-    # results.save()  # or .show()
+    results = model(img, size=640)
 
     df = results.pandas().xyxy[0]
-    food_name = list(df['name'])[0]
-    food_name = sc.get_translate(food_name)
-    # print(food_name)
+    foods = list(df['name'])
 
-    target_url = f'https://www.dietshin.com/calorie/calorie_search.asp?keyword={food_name}'
-    response = requests.get(target_url)
+    name_cal = {}
+
+    for food in foods :
+        food_name = sc.get_translate(food)
+
+        if "food_name" in name_cal:
+            name_cal["food_name"].append(food_name)
+        else:
+            name_cal["food_name"] = [food_name]
+
+        target_url = f'https://www.dietshin.com/calorie/calorie_search.asp?keyword={food_name}'
+
+        response = requests.get(target_url)
+
+        if response.status_code == 200:
+            html = response.text
+            soup = bs(html, 'html.parser')
+
+            # 검색결과 최상단 결과 가져오기
+            calorie = soup.select_one(
+                '#container > div.contents.calorieDc > table > tbody > tr:nth-of-type(1) > td:nth-of-type(2)')
+            print(food_name, str(calorie)[4:-5])
+            cal = (str(calorie)[4:-5])
+
+        else:
+            print(response.status_code)
+            cal = 'none'
+
+        if "calorie" in name_cal:
+            name_cal["calorie"].append(cal)
+        else:
+            name_cal["calorie"] = [cal]
+
+    name_cal = json.dumps(name_cal, ensure_ascii=False)
+    print(name_cal)
+
+    return name_cal
 
 
-df = results.pandas().xyxy[0]
-foods_name = list(df['name'])
-dic = {}
+@app.route('/', methods=['POST'])
+def recv_img() :
+    if request.method == "POST" :
+        imagefile = request.files['image']
 
-for food in foods_name:
-    
-    food_name = sc.get_translate(food_name)
-    # print(food_name)
-    
-    if "food_name" in dic:
-        dic["food_name"].append(food_name)
-    else:
-        dic["food_name"] = [food_name]
+        food_name = prediction(imagefile)
 
-    target_url = f'https://www.dietshin.com/calorie/calorie_search.asp?keyword={food_name}'
 
-    response = requests.get(target_url)
-
-    if response.status_code == 200:
-        html = response.text
-        soup = bs(html, 'html.parser')
-
-        # 검색결과 최상단 결과 가져오기
-        calorie = soup.select_one('#container > div.contents.calorieDc > table > tbody > tr:nth-of-type(1) > td:nth-of-type(2)')
-        print(food_name, str(calorie)[4:-5])
-        cal = (str(calorie)[4:-5])
-
-    else:
-        print(response.status_code)
-        cal = 'none'
-
-    if "calorie" in dic:
-        dic["calorie"].append(cal)
-    else:
-        dic["calorie"] = [cal]
-
-dic = json.dumps(dic, ensure_ascii=False)
-# print(dic)
-# print(is_json(dic))
+    return food_name
